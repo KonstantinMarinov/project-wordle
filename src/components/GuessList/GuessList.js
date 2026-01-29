8e114ece8f8f6d28a515a168502fbd4eb6777b37
Konstantin Marinov <konstantin.marinov@ascent.io>
2026-01-29 08:04:32 +0200

    Added win scenario

--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -3,14 +3,30 @@ import { answer } from "../Game/Game";
 
 export function GuessList({ guessList }) {
   const flippedGuessList = [...guessList].reverse();
+  const gameResult = calculateGameResult();
+  const numberOfGuesses = guessList.filter(
+    (guess) => guess.text.length > 0,
+  ).length;
 
+  function calculateGameResult() {
+    if (flippedGuessList[0].text.length > 0) {
+      const result = checkGuess(flippedGuessList[0].text, answer);
+      const isGuessCorrect = result.every(
+        (letter) => letter.status === "correct",
+      );
+
+      return isGuessCorrect ? "win" : undefined;
+    }
+
+    return undefined;
+  }
   function getClasses(row, column) {
-    if(flippedGuessList[row].text.length > 0) {
+    if (flippedGuessList[row].text.length > 0) {
       const result = checkGuess(flippedGuessList[row].text, answer);
       return `cell ${result[column].status}`;
     }
 
-    return "cell"
+    return "cell";
   }
 
   return (
@@ -35,6 +51,14 @@ export function GuessList({ guessList }) {
             </span>
           </p>
         ))}
+        {gameResult === "win" && (
+          <div className="happy banner">
+            <p>
+              <strong>Congratulations!</strong> Got it in {""}
+              <strong>{numberOfGuesses} guesses</strong>.
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
